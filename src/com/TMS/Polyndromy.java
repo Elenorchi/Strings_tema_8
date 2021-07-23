78525c216dbda0eb89dd70bff9de3fbd8c74729b
Elenorchi <eleenoree@gmail.com>
2021-07-23 11:46:18 +0200

    откорректировала задачу с палиндромами

--- src/com/TMS/Polyndromy.java
+++ src/com/TMS/Polyndromy.java
@@ -2,16 +2,20 @@ package com.TMS;
 
 public class Polyndromy {
     String string = "Он зашел в кабак и сделал заказ";
+    int a = 0;
 
     public void polyndrom() {
         String[] words = (string.split(" "));
         for (String word : words) {
             if (word.length() > 2) {
                 for (int i = 0; i < word.length() / 2; i++) {
-                    if (word.charAt(i) == word.charAt(word.length() - i -1)) {
-                        System.out.print(word + " ");
-                        break;
+                    if (word.charAt(i) == word.charAt(word.length() - i - 1)) {
+                        a++;
                     }
+                }
+                if (a == word.length()/2) {
+                    System.out.print(word + " ");
+                    a=0;
                 }
             }
         }
